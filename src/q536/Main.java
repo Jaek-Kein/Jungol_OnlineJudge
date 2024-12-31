0d7d05ab0ec5a1dd59df9f4540dfb849d42d5757
Jaewook Kim <yearsingle@naver.com>
2024-12-31 09:08:51 +0900

    upDATE

---
+++ src/q536/Main.java
@@ -0,0 +1,12 @@
+package q536;
+
+public class Main{
+    public static void main(String[] args){
+        int i = 1;
+        while (i<16){
+            if(i>1) System.out.print(' ');
+            System.out.print(i);
+            i++;
+        }
+    }
+}
