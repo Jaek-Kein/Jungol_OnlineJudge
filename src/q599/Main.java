6c97551975d9c1f866c2f497b4cdde6acf5d6d6d
Jaewook Kim <yearsingle@naver.com>
2025-01-03 09:32:05 +0900

    fix : 개행문자 수정

--- src/q599/Main.java
+++ src/q599/Main.java
@@ -10,7 +10,7 @@ public class Main {
 			String a = sc.nextLine();
 			String b = a.replaceAll("[^0-9]", "");
 			int c = Integer.parseInt(b);
-			System.out.print("\nASCII code =? ");
+			System.out.print("ASCII code =? ");
 			if (c < 33 || c > 127) break;
 			System.out.println((char)c);
 		}
