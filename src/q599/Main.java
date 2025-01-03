0a38d7d7910bea74bcd7ca9b939600cc28d818f4
Jaewook Kim <yearsingle@naver.com>
2025-01-03 09:27:10 +0900

    q599 added

---
+++ src/q599/Main.java
@@ -0,0 +1,18 @@
+package q599;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			String a = sc.nextLine();
+			String b = a.replaceAll("[^0-9]", "");
+			int c = Integer.parseInt(b);
+			System.out.print("\nASCII code =? ");
+			if (c < 33 || c > 127) break;
+			System.out.println((char)c);
+		}
+	}
+}
